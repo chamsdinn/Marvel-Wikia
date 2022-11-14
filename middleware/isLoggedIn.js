3e24d4ad3ab270462aad51bce6a637be2e81943b
chamsdinn <chams86@msn.com>
2022-11-14 18:34:41 +0100

    search bar done and auth route done

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,8 +1,10 @@
 module.exports = (req, res, next) => {
   // checks if the user is logged in when trying to access a specific page
   if (!req.session.currentUser) {
+    res.locals.isLoggedIn = false
     return res.redirect("/auth/login");
   }
-
+  res.locals.currentUser = req.session.currentUser
+  res.locals.isLoggedIn = true
   next();
 };
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const isLoggedIn = require('../middleware/isLoggedIn');
+const Character = require('../models/Character.model');
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+router.get("/profile", isLoggedIn, (req, res, next) => {
+	console.log(req.session.currentUser)
+	res.render("profile", { currentUser: req.session.currentUser })
+})
 
+router.get("/search", async(req, res, next)=>{
+  try {
+    const {superheroes} = req.query
+    
+    const searchQ = new RegExp(superheroes, "gi");
+    console.log(superheroes)
+    let searchedHero = [];
+    if (superheroes) {
+      searchedHero = await Character.find({name: searchQ})
+    }
+    res.render("search", {searchedHero})
+  } catch (error) {
+    console.log(error)
+  }
+})
 module.exports = router;
